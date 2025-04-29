5682cca16fcb0fcc67116f65c52deee4008ddfb9
Nathan Gross <nathan@bldg13.com>
2025-04-29 14:18:47 -0400

    publish and update email template files, add logo png, add vite plugin to copy image

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('build/images/logo.png') }}" class="logo" alt="Green Park Party">
+        </a>
+    </td>
+</tr>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     plugins: [
@@ -8,6 +9,14 @@ export default defineConfig({
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "resources/images/*",
+                    dest: "images",
+                },
+            ],
         }),
     ],
 });
